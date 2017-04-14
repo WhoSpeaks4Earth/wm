5ce62b2d098c6684feed3f408c4eb5e543abce07
Shawn Bahramy <shawnbahramy@gmail.com>
2017-04-14 10:18:23 -0400

    added exercise assessment page

---
+++ src/pages/exercise-ass/exercise-ass.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-exercise-ass',
+  templateUrl: 'exercise-ass.html'
+})
+export class ExerciseAssPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
