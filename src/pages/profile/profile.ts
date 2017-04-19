881147ac492ce297c00293d5970bc7f0005f3688
Shawn Bahramy <shawnbahramy@gmail.com>
2017-04-19 08:46:21 -0400

    user profile data stores in ionic cloud with success toast

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
 import { Auth, User } from '@ionic/cloud-angular';
 
 import { WelcomePage } from '../welcome/welcome';
@@ -13,20 +13,51 @@ export class ProfilePage {
   profile: { 
     firstName: string,
     lastName: string,
-    birthdate: Date } = {
+    birthdate: string,
+    gender: string,
+    weight: number,
+    heightFeet: number,
+    heightInches: number,
+    maritalStatus: string,
+    numKids: number,
+    zipcode: string } = {
       firstName: this.user.get('firstName', ''),
       lastName: this.user.get('lastName', ''),
-      birthdate: this.user.get('birthdate', '')
+      birthdate: this.user.get('birthdate', ''),
+      gender: this.user.get('gender', ''),
+      weight: this.user.get('weight', ''),
+      heightFeet: this.user.get('heightFeet', ''),
+      heightInches: this.user.get('heightInches', ''),
+      maritalStatus: this.user.get('maritalStatus', ''),
+      numKids: this.user.get('numKids', ''),
+      zipcode: this.user.get('zipcode', '')
   };
 
-  constructor(public navCtrl: NavController, public auth: Auth, public user: User) {
+  constructor(public navCtrl: NavController,
+              public toastCtrl: ToastController,
+              public auth: Auth,
+              public user: User) {
   }
 
   saveProfile() {
     this.user.set('firstName', this.profile.firstName);
     this.user.set('lastName', this.profile.lastName);
-    this.user.set('birthdate', this.profile.birthdate.getDate);
-    this.user.save();
+    this.user.set('birthdate', this.profile.birthdate);
+    this.user.set('gender', this.profile.gender);
+    this.user.set('weight', this.profile.weight);
+    this.user.set('heightFeet', this.profile.heightFeet);
+    this.user.set('heightInches', this.profile.heightInches);
+    this.user.set('maritalStatus', this.profile.maritalStatus);
+    this.user.set('numKids', this.profile.numKids);
+    this.user.set('zipcode', this.profile.zipcode);
+    this.user.save().then(() => {
+      let toast = this.toastCtrl.create({
+        message: 'Profile saved!',
+        duration: 3000,
+        position: 'top'
+      });
+      toast.present();
+    });
     console.log('profile saved');
   }
 
